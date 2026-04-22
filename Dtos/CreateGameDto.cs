eca4f32ba5820fa53e195706f99c9be629bf5c51
Borko14 <bobyturbotobg@gmail.com>
2026-04-22 15:14:29 +0300

    GameDatailsDto

--- Dtos/CreateGameDto.cs
+++ Dtos/CreateGameDto.cs
@@ -1,10 +1,11 @@
 using System.ComponentModel.DataAnnotations;
+using GameStore.Api.Models;
 
 namespace GameStore.Api.Dtos;
 
 public record CreateGameDto(
     [Required][StringLength(50)] string Name,
-    [Required][StringLength(20)] string Genre,
+    [Range(1, 15)] int GenreId,
     [Range(1, 100)]decimal Price,
     DateOnly ReleaseDate
 );
