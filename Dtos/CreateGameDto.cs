6cf39978645ca2a3108191fc39fc3bfcf129a7c2
Borko14 <bobyturbotobg@gmail.com>
2026-04-22 16:08:11 +0300

    Refactor Dtos

--- Dtos/CreateGameDto.cs
+++ Dtos/CreateGameDto.cs
@@ -5,7 +5,7 @@ namespace GameStore.Api.Dtos;
 
 public record CreateGameDto(
     [Required][StringLength(50)] string Name,
-    [Range(1, 15)] int GenreId,
+    [Range(1, 50)] int GenreId,
     [Range(1, 100)]decimal Price,
     DateOnly ReleaseDate
 );
